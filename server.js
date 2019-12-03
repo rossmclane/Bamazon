192ab167889e72248d57dca2f11d4c00d40b1185
Ross McLane <ross.mclane@gmail.com>
2019-12-02 21:36:33 -0500

    Bulk Create

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ var seedData = require("./seedData.js");
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-    db.Products.bulkCreate(seedData);
+    db.Product.bulkCreate(seedData);
   });
 });
