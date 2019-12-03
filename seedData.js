f581aeca90e9cc32ae720bb0e6bfa80fae2cd69b
Ross McLane <ross.mclane@gmail.com>
2019-12-02 21:40:58 -0500

    More Seed Data

--- seedData.js
+++ seedData.js
@@ -6,51 +6,45 @@ var seedData = [
     stock_quantity: 100
   },
   {
-    product_name: "Ball",
-    department_name: "Toys",
+    product_name: "Bat",
+    department_name: "Sports",
+    price: 75,
+    stock_quantity: 100
+  },
+  {
+    product_name: "Glove",
+    department_name: "Sports",
+    price: 100,
+    stock_quantity: 5
+  },
+  {
+    product_name: "Socks",
+    department_name: "Apparel",
+    price: 5,
+    stock_quantity: 100
+  },
+  {
+    product_name: "Pants",
+    department_name: "Apparel",
     price: 10,
     stock_quantity: 100
   },
   {
-    product_name: "Ball",
-    department_name: "Toys",
+    product_name: "Helmet",
+    department_name: "Sports",
+    price: 80,
+    stock_quantity: 100
+  },
+  {
+    product_name: "Chewing Gum",
+    department_name: "Candy",
     price: 10,
     stock_quantity: 100
   },
   {
-    product_name: "Ball",
-    department_name: "Toys",
-    price: 10,
-    stock_quantity: 100
-  },
-  {
-    product_name: "Ball",
-    department_name: "Toys",
-    price: 10,
-    stock_quantity: 100
-  },
-  {
-    product_name: "Ball",
-    department_name: "Toys",
-    price: 10,
-    stock_quantity: 100
-  },
-  {
-    product_name: "Ball",
-    department_name: "Toys",
-    price: 10,
-    stock_quantity: 100
-  },
-  {
-    product_name: "Ball",
-    department_name: "Toys",
-    price: 10,
-    stock_quantity: 100
-  },
-  {
-    product_name: "Ball",
-    department_name: "Toys",
-    price: 10,
+    product_name: "Cabbage",
+    department_name: "Produce",
+    price: 1,
     stock_quantity: 100
   }
 ];
